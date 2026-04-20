95016dd49a7c8efbecdee511d13c7ecffe49aa10
ShubhamCodeByte <shubhamsingh22401@gmail.com>
2026-04-20 15:06:17 -0400

    shell scripting

---
+++ 4.bash_scripting/arg_lab.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Script Name (\$0): $0"
+echo "First Arg (\$1): $1"
+echo "Second Arg (\$2): $2"
+echo "Total Count : $#"
+echo "All Arg (\$@): $@"
---
+++ 4.bash_scripting/myscript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This is a comment - the system ignores this
+NAME="Shubham"
+echo "Hello $NAME"
+read -p "Enter your current task: " TASK
+echo "You are currently: $TASK"
